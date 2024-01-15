c560d6335e91b0c9dfc308223af9a6e5a587beb0
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-01-14 23:57:09 -0500

    Agregado de archivos

---
+++ JS/Ruta.js
@@ -0,0 +1,4 @@
+const path = require('path');
+
+const directorioActual = path.join(process.cwd(), 'Analisis-de-datos', 'JS');
+console.log("Directorio actual en Node.js:", directorioActual);
--- JS/bugs.js
+++ JS/bugs.js
@@ -1,11 +1,16 @@
 // Importar las bibliotecas necesarias (cheerio y axios)
 // para poder realizar peticiones http a través de JavaScript.
+const fs = require('fs');
 const axios = require('axios'); 
 const cheerio = require('cheerio');
+// Para trabajar con rutas
+const path = require('path');
 
 // Definir variables necesarias
 const misterio="https://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
-// declarar un arreglo
+// Definir la ruta de trabajo actual
+const ruta = path.join(process.cwd(), 'Analisis-de-datos', 'JS');
+// Declarar un arreglo
 Dato_libro=[]
 // Funcion para realizar la peticion a la pagina web y obtener libros
 async function getLibros(url)
@@ -17,13 +22,15 @@ async function getLibros(url)
         const libros=$( "article");
         libros.each(function()
         {
-            titulo=$(this).find("h1 a").text();
+            titulo=$(this).find("h3 a").text();
             precio=$(this).find(".price_color").text();
             strock=$(this).find(".availability").text().trim();
 
             Dato_libro.push({titulo,precio,strock});
         });
         console.log(Dato_libro);
+        // Guardar en archivo CSV
+        guardarCSV(Dato_libro);
     }
     catch (error)
     {
@@ -31,5 +38,24 @@ async function getLibros(url)
         console.error("El error fue: ", error);
     }
 }
+
+function guardarCSV(datos) {
+    // Crear contenido CSV
+    const csvContent = "Título;Precio;Stock\n" +
+        datos.map(libro => `${libro.titulo};${libro.precio};${libro.strock}`).join("\n");
+
+    // Especificar la ruta del archivo CSV
+    const rutaArchivo = path.join(ruta,"libros.csv");
+
+    // Escribir en el archivo CSV
+    fs.writeFile(rutaArchivo, csvContent, 'utf8', (err) => {
+        if (err) {
+            console.error("Error al escribir en el archivo CSV:", err);
+        } else {
+            console.log(`Los datos se han guardado correctamente en ${rutaArchivo}`);
+        }
+    });
+}
+
 // llamada de la funcion para que se ejecute una vez cargado todo el html
 getLibros(misterio)
