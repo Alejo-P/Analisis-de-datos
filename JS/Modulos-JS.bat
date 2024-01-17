84308fc8627c0b7e3771a608778c883e67d4c1eb
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-01-16 20:37:02 -0500

    Deber S9

--- JS/Modulos-JS.bat
+++
@@ -1,5 +0,0 @@
-cd C:\Users\pinzo\OneDrive\Documentos\GitHub\Analisis-de-datos\JS
-npm init -y
-pause
-npm install cheerio axios
-pause
--- JS/Ruta.js
+++
@@ -1,4 +0,0 @@
-const path = require('path');
-
-const directorioActual = path.join(process.cwd(), 'Analisis-de-datos', 'JS');
-console.log("Directorio actual en Node.js:", directorioActual);
