24ee72a22c7d5689c7daaad94b36140dfa097139
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-01-16 23:17:43 -0500

    bat

---
+++ JS/Modulos-JS.bat
@@ -0,0 +1,7 @@
+@Echo off
+cd C:\Users\pinzo\OneDrive\Documentos\GitHub\Analisis-de-datos\JS
+echo inicializar node en la ruta actual
+echo npm init -y
+echo instalar las bibliotecas 'cheerio' y 'axios' en la ruta actual
+echo npm install cheerio axios
+pause
